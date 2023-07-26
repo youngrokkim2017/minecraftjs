633ae7ce1610c0c97f8c401b5f96d9feb896a21a
Patrick Kim <patrick.yrkim@gmail.com>
2023-07-26 14:15:10 -0700

    Add ground component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Canvas } from '@react-three/fiber';
 import { Sky } from '@react-three/drei';
+import { Physics } from '@react-three/cannon';
+
+import { Ground } from './components/Ground';
 
 function App() {
   return (
@@ -7,6 +10,10 @@ function App() {
       {/* <div>Outside Canvas</div> */}
       <Canvas>
         <Sky sunPosition={[100, 100, 20]} />
+        <ambientLight intensity={0.5} />
+        <Physics>
+          <Ground />
+        </Physics>
       </Canvas>
     </>
   );
