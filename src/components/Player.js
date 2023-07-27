1a13d568c4e231a836ad9755dc6b9548c36c3d7b
Patrick Kim <patrick.yrkim@gmail.com>
2023-07-27 15:31:15 -0700

    Add Player component

---
+++ src/components/Player.js
@@ -0,0 +1,36 @@
+import { useThree } from "@react-three/fiber"
+import { useSphere } from "@react-three/cannon"
+import { useEffect, useRef } from "react"
+import { Vector3 } from "three"
+
+export const Player = () => {
+    const { camera } = useThree()
+    const [ref, api] = useSphere(() => ({
+        mass: 1,
+        type: 'Dynamic',
+        position: [0, 1, 0]
+    }))
+
+    // store velocity
+    const vel = useRef([0, 0, 0])
+    useEffect(() => {
+        api.velocity.subscribe((v) => vel.current = v)
+    }, [api.velocity])
+
+    // store position of player
+    const pos = useRef([0, 0, 0])
+    useEffect(() => {
+        api.position.subscribe((p) => pos.current = p)
+    }, [api.position])
+
+    useFrame(() => {
+        camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]))
+    })
+
+        
+    return (
+        <mesh ref={ref}>
+
+        </mesh>
+    )
+}
