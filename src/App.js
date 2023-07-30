23d3e0c7b00a338fd0d990c539a6f91c6e66ed64
Patrick Kim <patrick.yrkim@gmail.com>
2023-07-30 15:02:53 -0700

    Add cube component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Physics } from '@react-three/cannon';
 import { Ground } from './components/Ground';
 import { Player } from './components/Player';
 import { FPV } from './components/FPV';
+import { Cubes } from './components/Cubes';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <FPV />
         <Physics>
           <Player />
+          <Cubes />
           <Ground />
         </Physics>
       </Canvas>
