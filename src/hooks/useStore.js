2c8d92b2f07695b985792a4a9b40fba4efdc794c
Patrick Kim <patrick.yrkim@gmail.com>
2023-07-31 00:29:18 -0700

    Remove test cubes

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -3,16 +3,7 @@ import { nanoid } from 'nanoid'
 
 export const useStore = create((set) => ({
     texture: 'dirt',
-    cubes: [{
-        key: nanoid(),
-        pos: [2, 0.5, 0],
-        texture: 'dirt',
-    },
-    {
-        key: nanoid(),
-        pos: [2, 0.5, 0],
-        texture: 'wood',
-    }],
+    cubes: [],
     addCube: (x, y, z) => {
         set((prev) => ({
             cubes: [
