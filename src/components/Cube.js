2690d93e4223bd99e778c40d264a3e66ec4c8a72
Patrick Kim <patrick.yrkim@gmail.com>
2023-07-31 00:33:47 -0700

    Add functionality to add and remove cube

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,6 +1,7 @@
 import { useBox } from "@react-three/cannon"
 
 import * as textures from "../images/textures"
+import { useStore } from "../hooks/useStore"
 
 export const Cube = ({ position, texture }) => {
 	const [ref] = useBox(() => ({
@@ -10,8 +11,45 @@ export const Cube = ({ position, texture }) => {
 
 	const activeTexture = textures[texture + 'Texture']
 
+    const [addCube, removeCube] = useStore((state) => [state.addCube, state.removeCube])
+
 	return (
-		<mesh ref={ref}>
+		<mesh
+			onClick={(e) => {
+				e.stopPropagation()
+				const clickedFace = Math.floor(e.faceIndex / 2)
+				const { x, y, z } = ref.current.position
+				if (e.altKey) {
+					removeCube(x, y, z)
+					return
+				}x
+				else if (clickedFace === 0) {
+					addCube(x + 1, y, z)
+					return
+				}
+				else if (clickedFace === 1) {
+					addCube(x - 1, y, z)
+					return
+				}
+				else if (clickedFace === 2) {
+					addCube(x, y + 1, z)
+					return
+				}
+				else if (clickedFace === 3) {
+					addCube(x, y - 1, z)
+					return
+				}
+				else if (clickedFace === 4) {
+					addCube(x, y, z + 1)
+					return
+				}
+				else if (clickedFace === 5) {
+					addCube(x, y, z - 1)
+					return
+				}
+			}} 
+            ref={ref}
+        >
 			<boxBufferGeometry attach="geometry" />
 			<meshStandardMaterial
 				map={activeTexture}
--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -16,7 +16,15 @@ export const useStore = create((set) => ({
             ]
         }))
     },
-    removeCube: () => {},
+    removeCube: (x, y, z) => {
+        set((prev) => ({
+			cubes: prev.cubes.filter(cube => {
+				const [X, Y, Z] = cube.pos
+				return X !== x || Y !== y || Z !== z
+			})
+
+		}))
+    },
     setTexture: () => {},
     saveWorld: () => {},
     resetWorld: () => {},
